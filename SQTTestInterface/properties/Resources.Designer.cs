6eab371ad364befb71bbafbda07015f69b473b4f
Benjamin Lucas <benjaminhmlucas@gmail.com>
2021-08-19 09:17:46 -0400

    Update properties folder

--- SQTTestInterface/properties/Resources.Designer.cs
+++ SQTTestInterface/properties/Resources.Designer.cs
@@ -59,5 +59,55 @@ namespace SQTTestInterface.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap robot1 {
+            get {
+                object obj = ResourceManager.GetObject("robot1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap robot2 {
+            get {
+                object obj = ResourceManager.GetObject("robot2", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap robot3 {
+            get {
+                object obj = ResourceManager.GetObject("robot3", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap robot4 {
+            get {
+                object obj = ResourceManager.GetObject("robot4", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap robot5 {
+            get {
+                object obj = ResourceManager.GetObject("robot5", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
